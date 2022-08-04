390e51f33059f41fc03a699e602caac8d3dfd4f4
frantecbh <franciscomenezesbh@gmail.com>
2022-08-04 18:14:58 -0300

    configurando debug

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,17 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.json())
+
 app.get("/", (request, response) => {
     return response.json({ message: "Ola Frantec" })
+})
+
+
+app.post("/courses", (request, response) => {
+    const { name } = request.body
+
+    return response.json({ name })
 })
 
 
