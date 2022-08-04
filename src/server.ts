baa30c438741c40632220a56405de6abba06b411
frantecbh <franciscomenezesbh@gmail.com>
2022-08-04 18:28:49 -0300

    criando rota categoria

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,18 @@
 import express from 'express'
+import { categoriesRoutes } from './routes/categories.routes'
 
 
 const app = express()
-
 app.use(express.json())
+
+app.use(categoriesRoutes)
 
 app.get("/", (request, response) => {
     return response.json({ message: "Ola Frantec" })
 })
 
 
-app.post("/courses", (request, response) => {
-    const { name } = request.body
 
-    return response.json({ name })
-})
 
 
 app.listen(3333, () => {
