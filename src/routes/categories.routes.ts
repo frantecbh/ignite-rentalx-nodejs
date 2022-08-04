587895401969510c3b3eb31ae2d9cff973690973
frantecbh <franciscomenezesbh@gmail.com>
2022-08-04 19:27:00 -0300

    create repository

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,35 +1,25 @@
 import { Router } from 'express'
+import { CategoriesRepository } from '../repositories/CategoriesRepository'
 
-import { v4 as uuidV4 } from 'uuid'
-import { Category } from '../model/Category'
+
+const categoriesRepository = new CategoriesRepository()
 
 
 const categoriesRoutes = Router()
 
-const categories: Category[] = []
+
 
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
-    const category = new Category()
-
-    Object.assign(category, {
+    categoriesRepository.create({
         name,
-        description,
-        created_at: new Date()
-
+        description
     })
 
-    // const category: Category = {
 
-    //     name,
-    //     description,
-    //     created_at: new Date()
-    // }
 
-    categories.push(category)
-
-    return response.status(201).json(categories)
+    return response.status(201).send()
 })
 
 
